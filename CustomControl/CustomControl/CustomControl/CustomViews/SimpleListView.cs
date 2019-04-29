2334986e40349f4a909ceef796e425c6cd8e50ce
neelakandan.kannan <neelakandan.kannan@syncfusion.com>
2019-04-29 16:05:54 +0530

    The modified project has been committed

---
+++ CustomControl/CustomControl/CustomControl/CustomViews/SimpleListView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using Xamarin.Forms;
+
+namespace CustomControl
+{
+    public class SimpleListView : ScrollView, IDisposable 
+    {
+        private ScrollController controller;
+        public int ItemHeight { get; set; }
+        public DataTemplate ItemTemplate { get; set; }
+
+        public static readonly BindableProperty DataSourceProperty =
+            BindableProperty.Create("DataSource", typeof(IList), typeof(SimpleListView), null, BindingMode.TwoWay);
+        public IList DataSource
+        {
+            get { return (IList)GetValue(DataSourceProperty); }
+            set { this.SetValue(DataSourceProperty, value); }
+        }
+
+        public SimpleListView()
+        {
+            this.Orientation = ScrollOrientation.Vertical;
+            this.ItemHeight = 60;
+            this.controller = new ScrollController() { ListView = this };
+            this.Scrolled += this.controller.ListView_Scrolled;
+            this.Content = this.controller;
+        }
+
+        public void Dispose()
+        {
+            this.Scrolled -= this.controller.ListView_Scrolled;
+        }
+    }
+}
